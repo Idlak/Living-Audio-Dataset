d9449b3a2a011ff274b596f6276fd93b2ed643de
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-07-16 10:23:20 +0100

    Changed English-language Wiktionary parser to meet specifications

--- tools/wiktionary2lex/parsers/en.py
+++ tools/wiktionary2lex/parsers/en.py
@@ -1,81 +1,127 @@
-# Parses the xml of all English language Wiktionary articles (found here: https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles.xml.bz2 )
-# Extracts all words from the specified language and their pronunciations
-# Usage is python en_wiktionary_parser.py [Wiktionary xml file] [Language to extract]
-# Language name is case sensitive and much match what Wiktionary uses
-# Words and their pronunciations are output to pronunciations_[language]_[timestamp].txt
-# Actions performed on all entries are logged in log_[language]_[timestamp].txt
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
 from lxml import etree as ET
 import sys
 import time
+import re
+import pickle
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+def data_stripping(xml):
+	if __debug__:
+		print "Starting parse"
+	tree = ET.parse(xml)
+	if __debug__:
+		print "Finished parse"
+	root = tree.getroot()
 
-if len(sys.argv) != 3:
-    print "Usage is python " + sys.argv[0] + " [Wiktionary xml file] [Language to extract]"
-    exit(1)
+	# Various patterns for matching the data we need to extract
+	language_pattern = re.compile("==([^=]+?)==\n")
+	pronunciation_pattern = re.compile("(.*?{{.*?\/(.*?)\/.*?}}.*)")
+	accent_set_pattern = re.compile("({{a.*?}})") # This one matches the set of braces that contains the accent data
+	accent_pattern = re.compile("\|(.*?)(?=[|}])")
+	POS_categories = ("Noun","Pronoun","Verb","Adjective","Adverb","Conjunction","Preposition","Interjection")
 
-print "Starting parse"
-tree = ET.parse(sys.argv[1])
-print "Finished parse"
-root = tree.getroot()
+	output_data = []
+	entry_POS = []
 
-language = sys.argv[2]
+	for index, entry in enumerate(root):
+		word = entry[0].text
+		if __debug__:
+			if index % 100000 == 0:
+				print "Entry " + str(index)
 
-# Create output files
-# Add timestamp to avoid overwriting results from other runs
-timestr = time.strftime("%Y%m%d-%H%M%S")
-pronunciations_filename = "pronunciations_" + language + "_" + timestr + ".txt"
-log_filename = "log_" + language + "_" + timestr + ".txt"
-pronunciations = open(pronunciations_filename,'w')
-log = open(log_filename,'w')
+		# The field containing the actual text for the article is always in entry[3]
+		for subentry in entry[3]:
 
-# Language headings on Wiktionary are in the form ==[Language]==
-language_section = "==" + language + "=="
+			# Matches the subentry containing the data we need
+			if subentry.tag == '{http://www.mediawiki.org/xml/export-0.10/}text':
 
-for index, entry in enumerate(root):
-	if index % 100000 == 0:
-		print "Entry " + str(index)
-	log.write(str(index) + " " + entry[0].text + "\n")
-
-	# The field containing the actual text for the article is always in entry[3]
-	for subentry in entry[3]:
-
-		# Matches the subentry containing the data we need
-		if subentry.tag == '{http://www.mediawiki.org/xml/export-0.10/}text':
-
-			# Some entries have empty text fields that break the parser otherwise
-			if subentry.text is None:
-				break
-
-			# Check that the article contains an entry for the relevant language
-			if language_section in subentry.text:
-
-				# Discard everything up to the name of the relevant language
-				discard, keep = subentry.text.split(language_section,1)
-
-				# ---- is the the marker for boundaries between languages
-				# Discard everthing after it if it exists
-				if "----" in keep:
-					keep, discard = keep.split("----",1)
-
-				# Discard everything up to the pronunciation section	
-				if "===Pronunciation===" in keep:
-					discard, keep = keep.split("===Pronunciation===",1)
-				else:
-					log.write("\t" + entry[0].text + " has no " + language + " pronunciation data\n")
+				# Some entries have empty text fields that break the parser otherwise
+				if subentry.text is None:
 					break
 
-				# Discard everything after the pronunciation section
-				# (The next section will always start with = if it exists)
-				if "\n=" in keep:
-					keep, discard = keep.split("\n=",1)
+				for language_match in re.finditer(language_pattern,subentry.text):
+					if __debug__:
+						print word + " has " + language_match.group(1) + " entry"
+					# Discard everything up to the name of the relevant language
+					discard, keep = subentry.text.split("==" + language_match.group(1) + "==",1)
 
-				# Discard first line for aesthetic purposes
-				discard, keep = keep.split("\n",1)
+					# ---- is the the marker for boundaries between languages
+					# Discard everthing after it if it exists
+					if "----" in keep:
+						keep, discard = keep.split("----",1)
+				
+					# Store the parts of speech that are mentioned in headings
+					del entry_POS[:]
+					for POS in POS_categories:
+						if "===" + POS + "===" in keep:
+							entry_POS.append(POS)
 
-				# Write word and pronunciation to file
-				pronunciations.write(entry[0].text + "\n" + keep + "\n----\n\n")
-				log.write("\t" + entry[0].text + " pronunciation written to file\n")
+					# If pronunciation section exists, isolate it
+					# If not move to the next language
+					if "===Pronunciation===" in keep:
+						discard, pronunciation_section = keep.split("===Pronunciation===",1)
+
+						# Discard everything after the pronunciation section
+						# (The next section will always start with = if it exists)
+						if "\n=" in pronunciation_section:
+							pronunciation_section, discard = pronunciation_section.split("\n=",1)
+
+						# Find pronunciation data
+						for pronunciation_match in re.finditer(pronunciation_pattern,pronunciation_section):
+							if __debug__:
+								print "\tFound pronunciation " + pronunciation_match.group(2)
+
+							# State variable used to ensure that an entry is added even if no accent data is available
+							no_accent_sets = 1
+
+							for accent_set_match in re.finditer(accent_set_pattern,pronunciation_match.group(1)):
+								if __debug__:
+									print "\t\tFound accent set " + accent_set_match.group(1)
+
+								no_accent_sets = 0
+								
+								for accent_match in re.finditer(accent_pattern,accent_set_match.group(1)):
+									if __debug__:
+										print "\t\t\tFound accent " + accent_match.group(1)
+
+									add_to_output(output_data,word,pronunciation_match.group(2),entry_POS,language_match.group(1),accent_match.group(1),None)
+
+							if no_accent_sets:
+								add_to_output(output_data,word,pronunciation_match.group(2),entry_POS,language_match.group(1),None,None)
+					else:
+						add_to_output(output_data,word,None,entry_POS,language_match.group(1),None,None)
+	return output_data
+
+def add_to_output(output,word,pronunciation,POS,language,accent,xsampa):
+	for entry in POS:
+		output.append({"word":word, "pron":pronunciation, "POS":entry, "lang":language, "accent":accent, "x-sampa":xsampa})
+	
+	# Adds an entry even if no part of speech data is available
+	if len(POS)==0:
+		output.append({"word":word, "pron":pronunciation, "POS":None, "lang":language, "accent":accent, "x-sampa":xsampa})
+
+def print_output(output):
+	for list_item in output:
+		for key in list_item:
+			if list_item[key] is None:
+				print key + ":None\t",
 			else:
-				log.write("\t" + entry[0].text + " is not a word in " + language + "\n")
+				print key + ":" + list_item[key] + "\t",
+		print "\n"
+
+if __name__ == "__main__":
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+
+	xml = sys.argv[1]
+
+	output = data_stripping(xml)
+	
+	if __debug__:
+		timestr = time.strftime("%Y%m%d-%H%M%S")
+		output_filename = "output/output_" + timestr + ".txt"
+		output_file = open(output_filename,'wb')
+		pickle.dump(output,output_file)
+		print_output(output)
