cbff736283a4fa539afd0c604cf18274a6b0d44d
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-07-16 10:53:56 +0100

    Changed tabs to spaces and added copyright section

--- tools/wiktionary2lex/parsers/en.py
+++ tools/wiktionary2lex/parsers/en.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+# Copyright 2018 Cereproc Ltd. (author: Caoimhín Laoide-Kemp)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
 from lxml import etree as ET
 import sys
 import time
