23bd8de9de831853241bf5d9fa7a0caf1aa17c2c
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-07-17 11:39:49 +0100

    Initial IPA to X-SAMPA implementation

--- tools/wiktionary2lex/wiktionary2lex.py
+++ tools/wiktionary2lex/wiktionary2lex.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
+from third_party import cxs
+
+def ipa2xsampa(ipa_text):
+    F = file('third_party/CXS.def')
+    cxs_table = cxs.make_table(F)
+    utf8_ipa_text = unicode(ipatext, 'utf8')
+    xsampa_text = cxs.ucipa2cxs(utf8_ipatext,cxs_table)
+    return xsampa_text
