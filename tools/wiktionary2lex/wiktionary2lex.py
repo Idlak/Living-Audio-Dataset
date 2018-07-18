d3b9e625a45ba2939e170da22ac897ac1bcff4c8
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-07-18 16:40:05 +0100

    Added functions to print entries with unique x-sampa and grapheme to lexicon

--- tools/wiktionary2lex/wiktionary2lex.py
+++ tools/wiktionary2lex/wiktionary2lex.py
@@ -4,6 +4,8 @@
 from third_party import cxs
 import imp
 import sys
+from lxml import etree as ET
+import itertools
 
 def ipa2xsampa(ipa_text,table):
     xsampa_text = cxs.ucipa2cxs(ipa_text,table)
@@ -30,9 +32,52 @@ def print_output(output):
                 print key + ":" + list_item[key] + "\t",
         print "\n"
 
+def output2lex(output,filename):
+    lexicon_output = open(filename,"wb")
+    lexicon = ET.Element("lexicon")
+    tree = ET.ElementTree(lexicon)
+
+    for list_item in output:
+        lex_entry = ET.SubElement(lexicon,"lex")
+        if list_item["pron"] is not None:
+            lex_entry.attrib["ipa"]=list_item["pron"]
+        if list_item["x-sampa"] is not None:
+            lex_entry.attrib["x-sampa"]=list_item["x-sampa"]
+        lex_entry.text=list_item["word"]
+
+    tree.write(
+            lexicon_output,pretty_print=True,   
+            xml_declaration=True,encoding="utf-8")
+
+def compare(dictionary1,dictionary2,*filters):
+    identical = 1
+    for key in filters:
+        if dictionary1[key] != dictionary2[key]:
+            identical = 0
+            break
+    return identical
+
+def filter_list(data,*filters):
+    filtered_data = []
+
+    for list_item in data:
+        list_item["unique"]=True
+
+    for first,second in itertools.combinations(data,2):
+        if compare(first,second,*filters):
+            second["unique"]=False
+
+    for list_item in data:
+        if list_item["unique"] is True:
+            filtered_data.append(list_item)
+    return filtered_data
+
 if __name__ == "__main__":
     language_code = sys.argv[1]
     wiktionary_xml = sys.argv[2]
-    output = xml2dict(language_code,xml)
+    output_lexicon = sys.argv[3]
+    output = xml2dict(language_code,wiktionary_xml)
     add_xsampa_to_data(output)
     print_output(output)
+    filtered_output=filter_list(output,"x-sampa","word")
+    output2lex(filtered_output,output_lexicon)
